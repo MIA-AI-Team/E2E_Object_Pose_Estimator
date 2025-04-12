116ea8c2a0f1674666e48b259e32b468d27bb20e
Ahmed Ayman <ahmedayman4a77@gmail.com>
2025-04-12 15:42:44 +0200

    fix save fig

--- train.py
+++ train.py
@@ -104,7 +104,7 @@ def main():
     plt.xlabel(f"Iteration (x {log_period})")
     plt.ylabel("Loss")
     plt.plot(loss_history)
-    plt.save("loss_history.png")
+    plt.savefig("loss_history.png")
 
 
 if __name__ == '__main__':
