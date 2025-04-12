b3d2382097ac88f3f9c719cf7fc8b6fd0088e1a8
Ahmed Ayman <ahmedayman4a77@gmail.com>
2025-04-12 15:18:32 +0200

    1 epoch

--- train.py
+++ train.py
@@ -70,7 +70,7 @@ def main():
     _iter = 0
 
     st_time = time.time()
-    for epoch in range(10):
+    for epoch in range(1):
         train_loss = []
         dataloader.dataset.dataset_type = 'train'
         for batch in dataloader:
